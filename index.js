d8717b695e45553fdeec1de29b114bf351d27c54
Palak Agrawal <palakagrawal899@gmail.com>
2026-03-06 21:52:49 +0530

    Added POST route to create new chats

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const Chat=require("./models/chat.js");
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
 
 main()
     .then(() => {
@@ -35,7 +36,38 @@ app.get("/chats", async (req, res) => {
     console.log(chats);
 
     res.render("index.ejs", { chats });
+
 });
+//NEW ROUTE
+app.get("/chats/new",async(req,res)=>{
+    res.render("new.ejs");
+});
+//CREATE ROUTE
+app.post("/chats",(req,res)=>{
+    let{ from,to,msg }=req.body;
+    let newChat=new Chat({
+        from :from,
+        to :to,
+        msg: msg,
+        created_at:new Date()
+    });
+
+    newChat
+        .save()
+        .then((res)=>{
+            console.log("chat was saved");
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    res.render("/chats");
+   
+
+});
+
+
+
+
 app.get("/",(req,res)=>{
     res.send("root is working");
 });
